4da853b59f3f919aef98cec4b950215d85a8db3b
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-09-23 23:37:54 +0200

    feat: instal HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { TodayPreviewComponent } from './today-preview/today-preview.component';
 import { TodayHighlightComponent } from './today-highlight/today-highlight.component';
 import { PreviousPreviewComponent } from './previous-preview/previous-preview.component';
 import { TodayPreviewEffects} from './today-preview/store/today-preview.effects';
+import { environment } from './../environments/environment';
 import * as fromApp from './store/app.reducer';
 
 @NgModule({
@@ -19,8 +22,10 @@ import * as fromApp from './store/app.reducer';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([TodayPreviewEffects])
+    EffectsModule.forRoot([TodayPreviewEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
