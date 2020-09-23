3b9477dff611652c49e14ef4c78d4e066523973a
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-09-23 23:34:44 +0200

    feat: find the coordinates from browser geolocation

--- src/app/today-preview/today-preview.component.ts
+++ src/app/today-preview/today-preview.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import { AppState } from '../store/app.reducer';
+import * as TodayPreviewActions from './store/today-preview.actions';
 
 @Component({
   selector: 'app-today-preview',
   templateUrl: './today-preview.component.html',
   styleUrls: ['./today-preview.component.scss']
 })
-export class TodayPreviewComponent implements OnInit {
+export class TodayPreviewComponent implements OnInit, OnDestroy {
+  private latt: number;
+  private long: number;
+  public error: string;
+  private storeSubscription: Subscription;
 
-  constructor() { }
+  constructor( private store: Store<AppState> ) { }
 
   ngOnInit(): void {
+    this.storeSubscription = this.store.select('todayPreview').subscribe( res => {
+      console.log(res);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.storeSubscription.unsubscribe();
+  }
+
+  onGeolocalisation() {
+      this.store.dispatch( new TodayPreviewActions.StartLocation() );
+      navigator.geolocation.getCurrentPosition( (position) => {
+        this.showPosition(position);
+      }, (error) => {
+        this.showError(error);
+      });
+    }
+
+  showPosition(position) {
+    this.latt = position.coords.latitude;
+    this.long = position.coords.longitude;
+    console.log(this.latt + ' ' + this.long);
+    this.store.dispatch( new TodayPreviewActions.FindLocationDetails({latt: this.latt, long: this.long}));
+  }
+
+  showError(error) {
+    if (error) {
+      this.error = 'It is not possible to geolocalize you. Please use the Search button to find tour position.';
+    }
   }
 
 }
