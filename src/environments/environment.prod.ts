d5fbc1bc300cf40649ce6ab5e843340ed6886a05
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-09-24 11:29:35 +0200

    feat: set the locationSearchApi variable into environments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  locationSearchApi: 'https://www.metaweather.com/api/location/search/?lattlong='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  locationSearchApi: 'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong='
 };
 
 /*
