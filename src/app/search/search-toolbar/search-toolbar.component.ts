35007c9e8d6968cab9167ba8d33d9bd7e8709b75
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-10-07 16:52:34 +0200

    feat: create the toolbar with x button to close the research and style it

---
+++ src/app/search/search-toolbar/search-toolbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { State } from './../store/search.reducer';
+import * as SearchActions from '../store/search.actions';
+
+@Component({
+  selector: 'app-search-toolbar',
+  templateUrl: './search-toolbar.component.html',
+  styleUrls: ['./search-toolbar.component.scss']
+})
+export class SearchToolbarComponent implements OnInit {
+
+  constructor(private store: Store<State>) { }
+
+  ngOnInit(): void {
+  }
+
+  onSearchClose() {
+    this.store.dispatch( new SearchActions.SearchModeOff());
+  }
+
+}
