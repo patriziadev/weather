cc43905748d88ab0a26c0ff8e61f3bdc3483df34
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-10-08 19:29:35 +0200

    feat: display and style the stored researches

---
+++ src/app/search/search-results/search-results.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import { AppState } from 'src/app/store/app.reducer';
+import * as SearchActions from './../store/search.actions';
+
+@Component({
+  selector: 'app-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent implements OnInit, OnDestroy {
+
+  public researchedLocations: string[];
+  private searchSubscription: Subscription;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.searchSubscription = this.store.select('search').subscribe( responseData => {
+      this.researchedLocations = responseData.savedResearches;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubscription.unsubscribe();
+  }
+
+  onSearch(location) {
+    this.store.dispatch(new SearchActions.SearchLocationDetails(location));
+  }
+}
