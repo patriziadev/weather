3e962ca0565f1f640caa8147b32ac4dc629a50c6
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-10-19 18:20:40 +0200

    style: add an anmation to open-close the search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit, OnDestroy, AfterContentInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { trigger, stagger, style, transition, animate } from '@angular/animations';
 
 import { AppState } from './store/app.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('changeLeftSide', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('400ms 200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('400ms 200ms ease-out', style({transform: 'translateX(-100%)'}))])
+      ])
+  ]
 })
 export class AppComponent implements OnInit, OnDestroy, AfterContentInit {
   title = 'What\'s the weather';
