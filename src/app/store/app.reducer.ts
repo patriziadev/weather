68301871fb97b08806e2fc3fe8bad9536d696148
Patrizia <patrizia@zaio.it>
2026-02-08 19:53:41 +0100

    chore: fix multiple vulnerabilities by updating to Angular 21

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,14 +1,14 @@
-import { ActionReducerMap } from '@ngrx/store/src';
+import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromTodayPreview from '../today-preview/store/today-preview.reducer';
 import * as fromSearch from '../search/store/search.reducer';
 
 export interface AppState {
-    todayPreview: fromTodayPreview.State;
-    search: fromSearch.State;
+  todayPreview: fromTodayPreview.State;
+  search: fromSearch.State;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
-    todayPreview: fromTodayPreview.TodayPreviewReducer,
-    search: fromSearch.SearchReducer
+  todayPreview: fromTodayPreview.TodayPreviewReducer,
+  search: fromSearch.SearchReducer,
 };
